7536396d0f214e15540e297751f3c5195e3cdaa5
Taejoon Kim <tkim00@stuy.edu>
2019-02-14 11:13:32 -0500

    started working on the constructor and the toString

---
+++ KnightBoard.java
@@ -0,0 +1,13 @@
+public class KnightBoard {
+  int[][] board;
+
+  public KnightBoard(int startingRows, int startingCols) {
+    board = new int[startingRows][startingCols];
+  }
+
+  public String toString() {
+    for (r = 0; r < board.length; r++) {
+      
+    }
+  }
+}
