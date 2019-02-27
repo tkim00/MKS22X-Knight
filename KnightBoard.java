949d77437ef9309b2bbe67b773ed9a79c2e93edf
Taejoon Kim <tkim00@stuy.edu>
2019-02-27 03:27:17 -0500

    countSolutions done and working

--- KnightBoard.java
+++ KnightBoard.java
@@ -57,4 +57,34 @@ public class KnightBoard {
     }
     return false;
   }
+
+  public int countSolutions(int startingRow, int startingCol) {
+    if (startingRow < 0 || startingCol < 0 || startingRow >= board.length || startingCol >= board[0].length) {
+      throw new IllegalArgumentException();
+    }
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board[r].length; c++) {
+        if (board[r][c] != 0) {
+          throw new IllegalStateException();
+        }
+      }
+    }
+    return countSolutionsH(startingRow, startingCol, 1);
+  }
+  public int countSolutionsH(int startingRow, int startingCol, int moveNumber) {
+    int sols = 0;
+    board[startingRow][startingCol] = moveNumber;
+    if (moveNumber == board.length * board[0].length) {
+      return 1;
+    }
+    for (int i = 0; i < 16; i+=2) {
+      if (startingRow+movement[i] >= 0 && startingRow+movement[i] < board.length &&
+      startingCol+movement[i+1] >= 0 && startingCol+movement[i+1] < board[0].length &&
+      board[startingRow+movement[i]][startingCol+movement[i+1]] == 0) {
+        sols += countSolutionsH(startingRow+movement[i], startingCol+movement[i+1], moveNumber+1);      
+        board[startingRow+movement[i]][startingCol+movement[i+1]] = 0;
+      }
+    }
+    return sols;
+  }
 }
