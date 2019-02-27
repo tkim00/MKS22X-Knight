151cf50eaf474575cb883caaff1e7020ca70d4a8
Taejoon Kim <tkim00@stuy.edu>
2019-02-27 03:06:35 -0500

    solve fixed and working

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,5 +1,6 @@
 public class KnightBoard {
   int[][] board;
+  int movement[] = {1, -2, 2, -1, 2, 1, 1, 2, -1, 2, -2, 1, -1, -2, -2, -1};
 
   public KnightBoard(int startingRows, int startingCols) {
     board = new int[startingRows][startingCols];
@@ -7,8 +8,8 @@ public class KnightBoard {
 
   public String toString() {
     String str = "";
-    for (r = 0; r < board.length; r++) {
-      for (c = 0; c < board[r].length; c++) {
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board[r].length; c++) {
         if (board[r][c] == 0) {
           str += "_ ";
         } else if (board[r][c] / 10 == 0) {
@@ -26,19 +27,34 @@ public class KnightBoard {
     if (startingRow < 0 || startingCol < 0 || startingRow >= board.length || startingCol >= board[0].length) {
       throw new IllegalArgumentException();
     }
-    for (r = 0; r < board.length; r++) {
-      for (c = 0; c < board[r].length; c++) {
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board[r].length; c++) {
         if (board[r][c] != 0) {
           throw new IllegalStateException();
         }
       }
     }
-    solveH(startingRow, startingCol, 1);
+    if (!solveH(startingRow, startingCol, 1)) {
+      board[startingRow][startingCol] = 0;
+      return false;
+    }
+    return true;
   }
-  public boolean solveH(int startingRow, startingCol, int moveNumber) {
-    if (moveNumber > startingRow * startingCol) {
+  public boolean solveH(int startingRow, int startingCol, int moveNumber) {
+    board[startingRow][startingCol] = moveNumber;
+    if (moveNumber == board.length * board[0].length) {
       return true;
     }
-
+    for (int i = 0; i < 16; i+=2) {
+      if (startingRow+movement[i] >= 0 && startingRow+movement[i] < board.length &&
+      startingCol+movement[i+1] >= 0 && startingCol+movement[i+1] < board[0].length &&
+      board[startingRow+movement[i]][startingCol+movement[i+1]] == 0) {
+        if (solveH(startingRow+movement[i], startingCol+movement[i+1], moveNumber+1)) {
+          return true;
+        }
+        board[startingRow+movement[i]][startingCol+movement[i+1]] = 0;
+      }
+    }
+    return false;
   }
 }
